1c85efa5966217c4bd39281f3496a372ac2389f9
Apaar <apaarsaini007@gmail.com>
2025-01-15 19:12:55 +0530

    Update spl_transfer.ts

--- ts/cluster1/spl_transfer.ts
+++ ts/cluster1/spl_transfer.ts
@@ -10,19 +10,26 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("GpxXfJR94cwFxZhKxrMnwqXxdur5YKR8qoUamjTLAK2j");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("pYb31c3sYzCnaEUhgRW9jM3ontTmmRKX6SF3o7T1tfy");
 
 (async () => {
     try {
         // Get the token account of the fromWallet address, and if it does not exist, create it
-
+        const fromAta = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey);
         // Get the token account of the toWallet address, and if it does not exist, create it
-
+        const toAta = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, to);
         // Transfer the new token to the "toTokenAccount" we just created
+        const decimals = 6; 
+        const amount = 1 * 10 ** decimals;
+        const tx = await transfer(connection, keypair, fromAta.address, toAta.address, keypair.publicKey, amount);
+        console.log(`Transfer successful! Transaction ID: ${tx}`);
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
 })();
+
+// Transfer successful! Transaction ID: xkibKyE6sWYK2rt2e8EAqyjYY3rhQZSYkYTQkVXtgFmdaPbu1V6HVFEYSVdxqkfGUx68Lt5YHX61sNg8YTA3fQV
+
